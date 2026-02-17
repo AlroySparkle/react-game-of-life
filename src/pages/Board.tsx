9c4c90aa90056670a9fe5903459d2f07e8b8a84a
Ali Alnasser <47244604+AlroySparkle@users.noreply.github.com>
2026-02-17 15:00:48 +0300

    first commit

---
+++ src/pages/Board.tsx
@@ -0,0 +1,171 @@
+import { useEffect, useRef, useState } from "react";
+import "./css/Board.css";
+
+type Cellprops = {
+  changeState: Function;
+  column: number;
+  row: number;
+  isAlive: boolean;
+};
+
+function Cell(props: Cellprops) {
+  return (
+    <div
+      key={props.row * 10 + " " + props.column}
+      onClick={() => {
+        props.changeState(props.row, props.column);
+      }}
+      style={{
+        background: props.isAlive ? "black" : "white",
+      }}
+      className={" h-12.5 w-12.5 block cursor-pointer"}
+    ></div>
+  );
+}
+export default function Board() {
+  const size = 50;
+  const [isAlive, setIsAlive] = useState<Record<string, boolean>>({});
+
+  const [intervalTime, setIntervalTime] = useState(1000);
+  const [runGenerations, setRunGenerations] = useState(true);
+
+  useEffect(() => {
+    const createIsAlive: { [key: string]: boolean } = {};
+    Array.from(Array(size)).forEach((_, row) =>
+      Array.from(Array(size)).forEach((_, column) => {
+        createIsAlive[row + "-" + column] = false;
+      }),
+    );
+    setIsAlive(createIsAlive);
+  }, []);
+
+  const nextGeneration = () => {
+    setIsAlive((prev) => {
+      const copy: { [key: string]: boolean } = { ...prev };
+      const neighborOffsets = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+      ];
+
+      Object.keys(copy).forEach((key) => {
+        // Extract row and column from "row-col"
+        const [row, col] = key.split("-").map(Number);
+
+        let aliveNeighbors = 0;
+
+        for (const [deltaRow, deltaCol] of neighborOffsets) {
+          const neighborRow = row + deltaRow;
+          const neighborCol = col + deltaCol;
+
+          if (neighborRow >= 0 && neighborCol >= 0) {
+            const neighborKey = `${neighborRow}-${neighborCol}`;
+            if (prev[neighborKey]) {
+              aliveNeighbors++;
+            }
+          }
+        }
+
+        copy[key] = aliveNeighbors === 3 || (aliveNeighbors === 2 && copy[key]);
+      });
+
+      return copy;
+    });
+  };
+
+  useEffect(() => {
+    let cancelled = false;
+
+    const loop = async () => {
+      while (!cancelled && runGenerations) {
+        await new Promise((res) => setTimeout(res, intervalTime));
+        if (!cancelled) nextGeneration();
+      }
+    };
+
+    loop();
+
+    return () => {
+      cancelled = true;
+    };
+  }, [runGenerations, intervalTime]);
+
+  const randomizeGrid = () => {
+    setIsAlive((prev) => {
+      const copy: { [key: string]: boolean } = { ...prev };
+      Object.keys(copy).forEach((key) => {
+        copy[key] = Math.random() >= 0.5;
+      });
+      return copy;
+    });
+  };
+  const changeState = (row: number, column: number): undefined => {
+    setIsAlive((prev) => {
+      const copy: { [key: string]: boolean } = { ...prev };
+      copy[row + "-" + column] = !copy[row + "-" + column];
+      return copy;
+    });
+  };
+  const board = Array.from(Array(size)).map((_, row) =>
+    Array.from(Array(size)).map((_, column) => (
+      <Cell
+        key={row * 10 + column}
+        row={row}
+        changeState={changeState}
+        column={column}
+        isAlive={isAlive[row + "-" + column] || false}
+      />
+    )),
+  );
+  return (
+    <div className="flex flex-col align-center gap-4">
+      <div
+        className="grid game-of-life-container"
+        style={{ gridTemplateColumns: `repeat(${size}, 50px` }}
+      >
+        {board}
+      </div>
+      <div className="flex flex-col gap-4">
+        <button
+          className="button"
+          onClick={() => {
+            randomizeGrid();
+          }}
+        >
+          randomize
+        </button>
+        <input
+          value={intervalTime}
+          onChange={(e) => {
+            const newTime = Number(e.target.value);
+            if (isNaN(newTime)) {
+              return;
+            }
+            setIntervalTime(newTime);
+          }}
+        />
+        <button
+          className="button"
+          onClick={() => {
+            setRunGenerations((prev) => !prev);
+          }}
+        >
+          {runGenerations ? "stop" : "start"}
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            nextGeneration();
+          }}
+        >
+          next gen
+        </button>
+      </div>
+    </div>
+  );
+}
